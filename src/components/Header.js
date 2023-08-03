9e9b060ce78d770a0f20f296ecfa9dcf5745123a
Hemant Verma <41631241+HemantVermaDU@users.noreply.github.com>
2023-08-03 09:05:57 +0530

    create-cart-page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,86 @@
-import { Container,Dropdown,FormControl, Navbar,Badge } from 'react-bootstrap';
-import {FaShoppingCart} from "react-icons/fa"
-import { CartState } from '../context/Context';
+import {
+  Container,
+  Dropdown,
+  FormControl,
+  Navbar,
+  Badge,
+  Button,
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+import { CartState } from "../context/Context";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
   const {
-    state : {cart},
+    state: { cart },
+    dispatch,
   } = CartState();
-
 
   return (
     <div>
-    <Navbar bg="dark" variant="dark" style={{ height:80}}>
+      <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
         <Container>
-            <Navbar.Brand>
-             <a href=''>Shopping Cart</a>
-            </Navbar.Brand>
-            <Navbar.Text className='search'>
-            <FormControl style={{ width:500}} className="m-auto" placeholder="name@example.com" />
-            </Navbar.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant='success'>
-                <FaShoppingCart color="white" fontSize="25px"/>
-                <Badge>{cart.length}</Badge>
-              </Dropdown.Toggle>
+          <Navbar.Brand>
+            <a href="">Shopping Cart</a>
+          </Navbar.Brand>
+          <Navbar.Text className="search">
+            <FormControl
+              style={{ width: 500 }}
+              className="m-auto"
+              placeholder="name@example.com"
+            />
+          </Navbar.Text>
+          <Dropdown className="dropdown" align={{ lg: "end" }}>
+            <Dropdown.Toggle variant="success">
+              <FaShoppingCart color="white" fontSize="25px" />
+              <Badge>{cart.length}</Badge>
+            </Dropdown.Toggle>
 
-              <Dropdown.Menu style={{minWidth:370}}>
-                <span style={{ padding:10 }}>Cart is Empty</span>
-              </Dropdown.Menu>
-            </Dropdown>
+            <Dropdown.Menu style={{ minWidth: 300 }}>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <span className="cartitem" key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>$ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() =>
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          })
+                        }
+                      />
+                    </span>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width:"95%", margin:"0 10px" }}>
+                      Go to Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty</span>
+              )}
+            </Dropdown.Menu>
+
+      
+          </Dropdown>
         </Container>
-    </Navbar>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/SingleProducts.js
+++ src/components/SingleProducts.js
@@ -1,58 +1,59 @@
-import {Card,Button} from "react-bootstrap";
-import Rating from './Rating';
-import {  CartState } from "../context/Context";
+import { Card, Button } from "react-bootstrap";
+import Rating from "./Rating";
+import { CartState } from "../context/Context";
 
 const SingleProducts = ({ prod }) => {
-
   const {
     state: { cart },
     dispatch,
   } = CartState();
 
-    // console.log(cart);
+  // console.log(cart);
 
   return (
-    <div className='products'>
+    <div className="products">
       <Card>
-        <Card.Img variant="top" src={prod.image} alt={prod.name}/>
+        <Card.Img variant="top" src={prod.image} alt={prod.name} />
         <Card.Body>
-        <Card.Title>{prod.name}</Card.Title>
-        <Card.Subtitle style={{ paddingBottom : 10 }}>
-    <span>$ {prod.price.split(".")[0]}</span>
-          {prod.fastDelivery ? (
-            <div>Fast Delivery</div>
-          ):(
-            <div>4 days Delivery</div>
-          )}
-          <Rating rating={prod.ratings} />
-        </Card.Subtitle>
-        {cart.some((p)=> p.id === prod.id) ? (
-             <Button onClick={()=>{
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>$ {prod.price.split(".")[0]}</span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 days Delivery</div>
+            )}
+            <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              onClick={() => {
                 dispatch({
-                  type : "REMOVE_FROM_CART",
-                  payload : prod,
-                })
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                });
               }}
-               variant="danger">Remove from Cart</Button>
-               ) : (
-              <Button onClick={() => {
+              variant="danger"
+            >
+              Remove from Cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
                 dispatch({
                   type: "ADD_TO_CART",
-                  payload : prod,
-                })
-              }} 
-              disabled={!prod.inStock}>
-        {!prod.inStock ? "out of Stock" : "Add to Cart"}
-        </Button>
-          )
-        }
-       
-       
+                  payload: prod,
+                });
+              }}
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? "out of Stock" : "Add to Cart"}
+            </Button>
+          )}
         </Card.Body>
-       
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default SingleProducts
+export default SingleProducts;
